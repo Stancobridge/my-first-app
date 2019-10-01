b4458531de2667e626e1854d270feedc7ea90cd8
stancobridge <stancoproof@gmail.com>
2019-10-01 20:23:57 +0100

    First assignment on component

---
+++ src/app/warnin-alert/warnin-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: '[warnin-alert]',
+  templateUrl: './warnin-alert.component.html',
+  styleUrls: ['./warnin-alert.component.css']
+})
+export class WarningAlertComponent {
+  
+}
